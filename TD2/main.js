e818d3b38d7ef27bab793cfd0e2c6c8f6204c632
Abdeljelil Malti <abdeljelil.malti@outlook.com>
2016-10-27 19:00:36 -0400

    Ajout des commentaires et remise du pourcentage a 0% lors du click sur annulation

--- TD2/main.js
+++ TD2/main.js
@@ -1,49 +1,73 @@
 var w;
-var isStartAlreadyClicked = false;
 
 $(document).ready(function(){
-	$("#compter").click(startWorker);
+	$("#compter").click(startWorker); //la fonction startWorker est appeler au clique de ce bouton
 
-	$("#cancel").attr("disabled", true);
-	$("#cancel").click(stopWorker);
+	$("#cancel").attr("disabled", true);//on ne doit pas pouvoir cliquer sur ce bouton au debut, sinon on va detruire un worker qui a deja ete detruit.
+	
+	$("#cancel").click(stopWorker); //la fonction stopWorker est appeler au clique de ce bouton
 });
 
+/***
+@nom : startWorker
+
+@description :  -fonction permettant d'appeler une autre fonction qui calcule le nombre de jetons dans le text-area via un worker
+				-de creer un webWorker
+				-d'envoyer le contenu du text-area au webWorker
+				-de modifier le pourcentage afficher en fonction de la progression du compte
+				-de modifier la taille de la bar de progression en fonction de la progression
+				-de modifier le fait de pouvoir appuyer sur un bouton ou non
+				-de prevenir si un navigateur n'est pas compatible avec les webWorker
+				
+***/
 function startWorker(){
 	if(typeof(Worker) !== "undefined") {
 		if(typeof(w) == "undefined") {
-			w = new Worker("worker.js");
+			w = new Worker("worker.js"); //Creation d'un objet qui permet de faire le lien entre le fichier worker.js et le main.js
 		}
 
-		w.postMessage($("#comment").val());
+		w.postMessage($("#comment").val()); //Envoie du text dans la text-area a worker.js
 
-		w.onmessage = function(event) {
-			$("#nombreJetons").html(event.data.words);
-			$("#progression").html(event.data.percentage + "%");
+		w.onmessage = function(event) { //Fonction qui permet de recuperer touts les elements renvoyer par worker.js
+			
+			$("#nombreJetons").html(event.data.words); //Modifie la compte du nombre de jetons qui se presentera au front-end
+			$("#progression").html(event.data.percentage + "%");//Modifie le pourcentage de la bar de progression.
 
-			$(".progress-bar").css("width", event.data.percentage + "%");
-			$("#progression").css("width", event.data.percentage + "%");
+			$(".progress-bar").css("width", event.data.percentage + "%"); //Modifie la taille de la bar de progression en bleu.
+			$("#progression").css("width", event.data.percentage + "%"); //Modifie la position du pourcentage #progression dans la bar de progression.
 
-			$("#compter").attr("disabled", true);
-			$("#cancel").attr("disabled", false);
-			isStartAlreadyClicked = true;
+			$("#compter").attr("disabled", true); //Rend impossible d'appuyer sur le bouton 2 fois
+			$("#cancel").attr("disabled", false); //Rend possible d'annuler le compte en cours
+
 		};
 	} else {
-		$("#nombreJetons").html("Votre navigateur ne supporte pas les Web Workers...");
+		$("#nombreJetons").html("Votre navigateur ne supporte pas les Web Workers..."); //Si un navigateur web ne reconnait pas les web workers.
 	}
 }
 
-function stopWorker(){
-	if(isStartAlreadyClicked)
-		{
-			$("#nombreJetons").text("Annuler");
-			w.terminate();
-    		w = undefined;
-    		isStartAlreadyClicked = false;
-    		
-			$(".progress-bar").css("width", 0 + "%");
-			$("#progression").css("width", 0 + "%");
-		}
+/***
+@nom : stopWorker
+
+@description :  -fonction de detruire le webworker et de le set a undefined
+				-de reset les proprietes de la barre de progression a 0%
+				-d'empecher de reappuyer sur le bouton cancel
+				-de permet de cliquer sur le bouton compter
+***/
+
+function stopWorker()
+{
+	$("#nombreJetons").text("Annuler"); //Text a afficher dans le cas ou on annule le compte
+	w.terminate(); //on met fin au worker
+	w = undefined; //on met le worker a undefined afin de pouvoir le recreer dans startWorker plutard
+
+//on reinitialise les donnees de la bar de progression pour eviter un effet elastique lorsqu'on reprend le compte plustard
+	$(".progress-bar").css("width", 0 + "%"); //La taille de la bar de progression est reinitialiser a zero.
+	$("#progression").css("width", 0 + "%"); //La position du pourcentage #progression dans la bar de progression est remis a zero.
+	
+	$("#progression").html(0 + "%");//le pourcentage est mis a 0%
+
+	//Apres avoir annuler, on ne doit pas pouvoir recliquer sur ce bouton, sinon on va detruire un worker qui a deja ete detruit.
 	$("#cancel").attr("disabled", true);
-	$("#compter").attr("disabled", false);
+	$("#compter").attr("disabled", false); //Et on doit pouvoir reappuyer sur le bouton compter.
 	
 }
--- TD2/worker.js
+++ TD2/worker.js
@@ -1,24 +1,20 @@
-self.importScripts("CompteurJetons.js");
-var compteur = new CompteurJetons();
+self.importScripts("CompteurJetons.js"); //On importe le script fournit
+var compteur = new CompteurJetons(); //Creation d'un objet du script fournit
 
-self.addEventListener('message', function(e) {
+self.addEventListener('message', function(e) //Creation d'un methode qui fait la communication entre le fichier main.js et compteurJetons.js
+{
 	
-	var gen = compteur.compterJetons(e.data);
+	var gen = compteur.compterJetons(e.data); //Creation d'un objet compteurJetons qui contient les methodes qui permettent de faire le compte que l'on a besoin.
 	var totalWords = 0;
 	var pourcent = 0;
 
-	while(gen.next().value != undefined){
-		totalWords = compteur.getJetons();
-		pourcent = compteur.getProgress();
-		postMessage({
+	while(gen.next().value != undefined)
+	{
+		totalWords = compteur.getJetons(); //on recupere la contiter de jetons
+		pourcent = compteur.getProgress(); //on recupere le pourcentage de compte effectuer
+		postMessage({ //on envoie ces 2 donnees dans le main.js
 			"words" : totalWords,
 			"percentage" : pourcent
 		});
-			
-
-		
-		//console.log(pourcent);
 	}
-	
-   
 }, false);
